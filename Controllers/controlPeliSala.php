ad972db8e6897724a4b61658a6a66009594a3905
Itati Huayamave Swett <itatiswett@gmail.com>
2026-03-16 21:31:53 -0500

    descripcion d ela funcion

--- Controllers/controlPeliSala.php
+++ Controllers/controlPeliSala.php
@@ -4,10 +4,21 @@ require_once("../PruebaTecnica2.0/Services/logicaPeliSala.php");
     class controlPeliSala {
         private $serPeliSala;
         
+        //instancia del objecto que nos permitira acceder las funciones de la lógica
         public function __construct(){
             $this->serPeliSala = new logicaPeliSala();
         }
 
+        /**
+         * Undocumented function
+         *
+         * @param [type] $fecha_publicacion
+         * @return void
+         * 
+         * esta función permite evaludar que los datos que van a traer mediante la fecha sean correctos, se traen
+         * mediante un array que se evalua la cantidad de datos para que se pueda mostrar los datos o dar errores en caso de que
+         * ven vacios.
+         */
         public function ListarPeliFecha($fecha_publicacion){
                 $peliculas=$this->serPeliSala->ObtenerPeliFecha($fecha_publicacion);
 
@@ -22,6 +33,15 @@ require_once("../PruebaTecnica2.0/Services/logicaPeliSala.php");
                 }
         }
 
+        /**
+         * Undocumented function
+         *
+         * @param string $nombre
+         * @return void
+         * 
+         * esta función permite traer los datos de la sala y da un error en caso de que al llamar a la función del servicio
+         * no muestre los datos con las salidas esperadas.
+         */
         public function DisponibilidadSala(string $nombre){
             try {
             return $this->serPeliSala->ObtenerSalaNombre($nombre);
